431b17945d30bb74a5c952439168e83fae1ebc98
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-01-25 03:02:30 +0100

    Added hashing utility

---
+++ src/pba/util/hash.hpp
@@ -0,0 +1,17 @@
+// hash.hpp
+#pragma once
+
+#include "pba/core_types.hpp"
+
+namespace ds_pba
+{
+// This is based on the boost implementation of hash_mix
+// https://www.boost.org/doc/libs/1_84_0/boost/container_hash/detail/hash_mix.hpp
+inline constexpr usize k_hash_combine_constant = 0x9e3779b9zu;
+
+[[nodiscard]] inline usize hash_combine_seed(usize seed, usize v) noexcept
+{
+    seed ^= v + k_hash_combine_constant + (seed << 6) + (seed >> 2);
+    return seed;
+}
+}  // namespace ds_pba
